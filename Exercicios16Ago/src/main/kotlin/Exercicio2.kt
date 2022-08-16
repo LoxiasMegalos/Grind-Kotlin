290ebeb970e05ee0e8a887f18895efe6f76a544d
LoxiasMegalos <murillo.alkantara@gmail.com>
2022-08-16 15:19:10 -0300

    Finalizando Exercicios

--- Exercicios16Ago/src/main/kotlin/Exercicio2.kt
+++ Exercicios16Ago/src/main/kotlin/Exercicio2.kt
@@ -10,17 +10,17 @@ fun main(){
     println("Infome o terceiro numero: ")
     val n3 = readln().toInt()
 
-    if(n1 > n2 && n1 > n3 && n2 > n3){
-        println("$n1 $n2 $n3")
-    } else if( n1 > n2 && n1 > n3 && n3 > n2){
-        println("$n1 $n3 $n2")
-    } else if( n2 > n1 && n2 > n3 && n1 > n3){
-        println("$n2 $n1 $n3")
-    }else if( n2 > n1 && n2 > n3 && n3 > n1){
-        println("$n2 $n3 $n1")
-    }else if( n3 > n2 && n3 > n1 && n1 > n2){
-        println("$n3 $n1 $n2")
-    }else if( n3 > n2 && n3 > n1 && n2 > n1){
+    if(n1 >= n2 && n2 >= n3){
         println("$n3 $n2 $n1")
+    }else if( n1 >= n3 && n3 >= n2){
+        println("$n2 $n3 $n1")
+    } else if( n2 >= n1 && n1 >= n3){
+        println("$n3 $n1 $n2")
+    }else if( n2 >= n3 && n3 >= n1){
+        println("$n1 $n3 $n2")
+    }else if( n3 >= n1 && n1 >= n2){
+        println("$n2 $n1 $n3")
+    }else if( n3 >= n2 && n2 >= n1){
+        println("$n1 $n2 $n3")
     }
 }
